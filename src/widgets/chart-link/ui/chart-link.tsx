86805535578ce94c21ad7194a4e83ae1840ad196
Savchenko Alexander <asavchenko@datatech.ru>
2023-05-04 22:02:52 +0400

    link to linkout

--- src/widgets/chart-link/ui/chart-link.tsx
+++ src/widgets/chart-link/ui/chart-link.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react'
 
+import { getLinkOutProps } from '~/entities/decision/ui/node/lib/get-link-out-position'
 import { useForceUpdate } from '~/utils/hooks'
 import { EventNames, State } from '~/widgets/chart-item'
 
@@ -17,15 +18,25 @@ export default function ChartLink<T>(props: ChartLinkProps<T>): JSX.Element {
     props.sourceState.mitt.on(EventNames.setPosition, update)
   })
 
-  return <path stroke='black' d={drawPath()} />
+  return <path stroke='black' d={drawPath()} strokeWidth={2} />
 
   // Private
 
   function drawPath(): string {
-    const sx = props.sourceState.position.x + props.sourceState.width / 2 - 20
+    let sx = props.sourceState.position.x + props.sourceState.width / 2 - 20
     const sy = props.sourceState.position.y + props.sourceState.height / 2
     const tx = props.targetState.position.x
     const ty = props.targetState.position.y
+
+    if (props.link.type === 'true') {
+      const pos = getLinkOutProps({ width: props.sourceState.position.x + props.sourceState.width / 2, height: 0 }, 0)
+      sx = pos.cx
+    }
+    if (props.link.type === 'false') {
+      const pos = getLinkOutProps({ width: props.sourceState.position.x + props.sourceState.width / 2, height: 0 }, 1)
+      sx = pos.cx
+    }
+
     return `M${sx},${sy}L${tx},${ty}`
   }
 }
