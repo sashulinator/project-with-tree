fc2e9a7cf276367c921af0bd287b498ad56a0468
Savchenko Alexander <asavchenko@datatech.ru>
2023-05-12 12:09:40 +0400

    getOffsetInElement

--- src/widgets/chart-link/ui/chart-link.tsx
+++ src/widgets/chart-link/ui/chart-link.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useLayoutEffect } from 'react'
 
+import { getOffsetInElement } from '~/utils/dom/get-offset-in-element'
 import { useForceUpdate } from '~/utils/hooks'
 import { EventNames, State } from '~/widgets/chart-item'
 
@@ -25,20 +26,12 @@ export default function ChartLink<T>(props: ChartLinkProps<T>): JSX.Element {
   // Private
 
   function drawPath(): string {
+    const srcLinkEl = props.sourceState.ref.current?.querySelector(`[data-id="${props.link.id}"]`) as HTMLElement
+    const srcLinkOffset = getOffsetInElement(srcLinkEl, srcLinkEl?.parentElement)
     const sx = props.sourceState.position.x + props.sourceState.width / 2
-    const sy = props.sourceState.position.y - props.sourceState.height / 2 + getOffset()
+    const sy = props.sourceState.position.y - props.sourceState.height / 2 + srcLinkOffset.top
     const tx = props.targetState.position.x - props.targetState.width / 2
     const ty = props.targetState.position.y
-
     return `M${sx},${sy}L${tx},${ty}`
-  }
-
-  function getOffset(): number {
-    if (props.sourceState.ref.current === null) return 0
-    const linkOutEl = props.sourceState.ref.current.querySelector(`[data-id="${props.link.id}"]`) as HTMLElement
-    const parentOffset = linkOutEl?.parentElement?.offsetTop || 0
-    const offsetTop = linkOutEl.offsetTop - parentOffset
-    console.log(offsetTop)
-    return offsetTop
   }
 }
