067204d8517ee715468254ab3d9fb3e4cd53728a
Savchenko Alexander <asavchenko@datatech.ru>
2023-07-04 10:27:22 +0400

    rename css vars

--- src/entities/point/ui/node/widgets/joint/types/css-vars.ts
+++ src/entities/point/ui/node/widgets/joint/types/css-vars.ts
@@ -1,7 +1,7 @@
 import { ToStringable } from '~/utils/core'
 
 export interface CSSVars {
-  pointJoint_bg__unlinked: ToStringable
-  pointJoint_bg__linked: ToStringable
-  pointJoint_bg__new: ToStringable
+  point_nodeJoint_bg__unlinked: ToStringable
+  point_nodeJoint_bg__linked: ToStringable
+  point_nodeJoint_bg__new: ToStringable
 }
