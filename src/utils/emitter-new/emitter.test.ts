d6eaedcd2e7ffdcceed28e311f8d1da130b9373b
Savchenko Alexander <asavchenko@datatech.ru>
2023-07-14 15:55:18 +0400

    add emitter

--- src/utils/emitter-new/emitter.test.ts
+++ src/utils/emitter-new/emitter.test.ts
@@ -1,9 +1,11 @@
-import { Emitter } from './emitter'
+import { Emitter } from './event-emitter'
+import { Prop } from './prop'
 
 type Events = {
   first: { objectType: boolean }
   second: string
   third: number
+  name: { value: number }
 }
 
 describe(Emitter.name, () => {
@@ -32,5 +34,11 @@ describe(Emitter.name, () => {
     emitter.off('second', listener)
 
     expect(emitter.listeners.second?.length).toBe(0)
+  })
+
+  it('off', () => {
+    const emitter = new Emitter<Events>()
+
+    const prop = new Prop('name', 1111).on((p) => emitter.emit(p.name, p))
   })
 })
--- src/utils/emitter-new/emitter.ts
+++ src/utils/emitter-new/emitter.ts
@@ -1,64 +1,45 @@
-import { Events } from './types/events'
 import { Listener } from './types/listener'
-import { ListenerDictionary } from './types/listener-dictionary'
 
 /**
  * @class Emitter
- * @property {ListenerDictionary<TEvents> | undefined} listeners
+ * @property { Listener<TEvents>[]} listeners
  */
-export class Emitter<TEvents extends Events> {
-  listeners: ListenerDictionary<TEvents>
+export class Emitter<TValue extends unknown> {
+  listeners: Listener<TValue>[]
 
   /**
    * @constructor
-   * @param {ListenerDictionary<TEvents>} [listeners]
+   * @param {ListenerDictionary<TValue>} [listeners]
    */
-  constructor(listeners?: ListenerDictionary<TEvents> | undefined) {
-    this.listeners = listeners || {}
+  constructor(listeners?: Listener<TValue>[] | undefined) {
+    this.listeners = listeners ?? []
   }
 
   /**
-   * Subscribes on event specified function
-   * @template {keyof TEvents} T names
-   * @param {T} eventName Event name
-   * @param {Listener<TEvents[T]>} listener listener
+   * Subscribes listener.
+   * @param {Listener<TValue[T]>} listener listener
    */
-  on = <T extends keyof TEvents>(eventName: T, listener: Listener<TEvents[T]>): (() => void) => {
-    const eventListenerList = this.listeners[eventName]
+  add = (listener: Listener<TValue>): (() => void) => {
+    this.listeners.push(listener)
+    return () => this.remove(listener)
+  }
 
-    if (!eventListenerList) {
-      this.listeners[eventName] = [listener]
-    } else {
-      eventListenerList?.push(listener)
+  /**
+   * Removes listener.
+   * @param {Listener<TValue[T]>} listener listener
+   */
+  remove = (listener: Listener<TValue>) => {
+    this.listeners?.splice(this.listeners.indexOf(listener) >>> 0, 1)
+  }
+
+  /**
+   * Emits all listeners.
+   * @param {TValue[T]} value Event
+   */
+  emit = (value: TValue): void => {
+    for (let index = 0; index < this.listeners.length; index++) {
+      const listener = this.listeners?.[index]
+      listener?.(value)
     }
-
-    return () => this.off(eventName, listener)
-  }
-
-  /**
-   * Invokes all handlers for the given event name.
-   * @template {keyof TEvents} T names
-   * @param {T} eventName Event name
-   * @param {TEvents[T]} event Event
-   */
-  emit = <T extends keyof TEvents>(eventName: T, event: TEvents[T]): void => {
-    const eventListenerList = this.listeners[eventName]
-    const length = eventListenerList?.length || 0
-
-    for (let index = 0; index < length; index++) {
-      const listener = eventListenerList?.[index]
-      listener?.(event)
-    }
-  }
-
-  /**
-   * Removes an event handler for the given type.
-   * @template {keyof TEvents} T names
-   * @param {T} eventName Event name
-   * @param {Listener<TEvents[T]>} listener listener
-   */
-  off = <T extends keyof TEvents>(eventName: T, listener: Listener<TEvents[T]>) => {
-    const eventListenerList = this.listeners[eventName]
-    eventListenerList?.splice(eventListenerList.indexOf(listener) >>> 0, 1)
   }
 }
