a81da7e16424d69fbcf723ebe51e56343cd0c9e3
Savchenko Alexander <asavchenko@datatech.ru>
2023-07-18 12:20:59 +0400

    mention ref

--- src/ui/mentions/ui/mentions.tsx
+++ src/ui/mentions/ui/mentions.tsx
@@ -1,44 +1,37 @@
-import { useState } from 'react'
-import { MentionsInput, Mention } from 'react-mentions'
+import { MentionsInput as RMMentionsInput, MentionsInputProps } from 'react-mentions'
 import defaultStyle from './defaultStyle'
-import defaultMentionStyle from './defaultMentionStyle'
-import { IMentionsProps } from '../types/types'
+
 import Field from '~/abstract/field/ui/field'
 import './mentions.css'
-import { useControlledState } from '~/utils/hooks/controlled-state'
 
-export default function Mentions({ data, ...props }: IMentionsProps): JSX.Element {
-  const onAdd = (e: string | number): void => {
-    console.dir(e)
-  }
+import { FieldProps } from '~/ui/field'
+import { fns } from '~/utils/function'
 
-  const { isFocused, disabled, focusedChange, defaultFocus, value, onChangeValue } = props
+import { useBoolean } from '~/utils/hooks'
+import { c } from '~/utils/core'
 
-  const [focus, setFocus] = useControlledState<boolean>(defaultFocus || false, isFocused, focusedChange)
+MentionInput.displayName = 'ui-Mentions'
+
+export interface MentionsProps extends MentionsInputProps {
+  fieldProps?: FieldProps
+  isError?: boolean | undefined
+  transparent?: boolean | undefined
+}
+
+export default function MentionInput(props: MentionsProps): JSX.Element {
+  const { fieldProps, isError, ...mentionInputProps } = props
+
+  const [isFocused, focus, blur] = useBoolean(false)
 
   return (
-    <Field {...props}>
-      <MentionsInput
-        disabled={!!disabled}
-        onFocus={toggleFocused}
-        onBlur={toggleBlur}
-        value={value}
-        onChange={onChangeValue}
-        style={defaultStyle}
-        className='ui-Mentions'
-      >
-        <Mention onAdd={onAdd} trigger='@' data={data} style={defaultMentionStyle} />
-      </MentionsInput>
+    <Field isError={isError} isFocused={isFocused} disabled={mentionInputProps.disabled} {...fieldProps} height={null}>
+      <RMMentionsInput
+        {...mentionInputProps}
+        onFocus={fns(props.onFocus, focus)}
+        onBlur={fns(props.onBlur, blur)}
+        style={{ ...defaultStyle }}
+        className={c(props.className, MentionInput.displayName)}
+      />
     </Field>
   )
-
-  // Private
-
-  function toggleFocused(): void {
-    if (!focus) setFocus(true)
-  }
-
-  function toggleBlur(): void {
-    if (focus) setFocus(false)
-  }
 }
