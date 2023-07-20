37b1a9bdff4db2588b91a0ac5fc777bea31b6512
Savchenko Alexander <asavchenko@datatech.ru>
2023-07-20 11:02:57 +0400

    storybook canvas ref

--- src/pages/storybook/section.tsx
+++ src/pages/storybook/section.tsx
@@ -4,6 +4,7 @@ import Flex from '~/abstract/flex'
 
 interface SectionProps {
   header?: React.ReactNode
+  toolbar?: React.ReactNode
   children: React.ReactNode
 }
 
@@ -11,6 +12,8 @@ export default function Section(props: SectionProps): JSX.Element {
   return (
     <Flex dir='column'>
       {props.header}
+      <hr style={{ width: '100%', opacity: '.2' }} />
+      {props.toolbar}
       <Flex>
         <Flex width='50%' padding='2rem 2rem 2rem 0'>
           {props.children}
