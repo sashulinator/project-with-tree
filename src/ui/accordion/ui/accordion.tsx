7f72c61f9e64e81a24c8f8a1d1c8efa1bc0070c2
Savchenko Alexander <asavchenko@datatech.ru>
2023-07-22 13:24:33 +0400

    create ui-Accordion

--- src/ui/accordion/ui/accordion.tsx
+++ src/ui/accordion/ui/accordion.tsx
@@ -1,45 +1,10 @@
-import Button from '~/abstract/button/ui/button'
-import Collapse, { CollapseProps } from '~/abstract/collapse/ui/collapse'
-
-import './accordion.css'
+import AbstractAccordion, { AccordionProps } from '~/abstract/accordion'
 import { c } from '~/utils/core'
-import { ChevronRight } from '~/ui/icon'
-import { useControlledState } from '~/utils/hooks/controlled-state'
 
-CollapseUI.displayName = 'ui-Collapse'
+Accordion.displayName = 'ui-Accordion'
 
-interface Props extends Omit<CollapseProps, 'isExpanded'> {
-  title: string
-  isExpanded?: boolean | undefined
-  onExpandedChange?: ((value: boolean) => void) | undefined
-  defaultExpanded?: boolean | undefined
-}
+export type { AccordionProps }
 
-export default function CollapseUI(props: Props): JSX.Element {
-  const { title, ...collapseProps } = props
-
-  const [isExpanded, setExpanded] = useControlledState<boolean>(
-    props.defaultExpanded || false,
-    props.isExpanded,
-    props.onExpandedChange
-  )
-
-  return (
-    <div style={props.rootProps?.style} className={c(CollapseUI.displayName, props.rootProps?.className)}>
-      <Button className='header-collapse' onClick={toggleExpanded}>
-        <div>{title}</div>
-        <ChevronRight className={isExpanded ? 'arrow-icon rotate' : 'arrow-icon'} />
-      </Button>
-
-      <Collapse {...collapseProps} rootProps={{ className: 'body-Collapse' }} isExpanded={isExpanded}>
-        {props.children}
-      </Collapse>
-    </div>
-  )
-
-  // Private
-
-  function toggleExpanded(): void {
-    setExpanded((item) => !item)
-  }
+export default function Accordion<HeaderProps>(props: AccordionProps<HeaderProps>): JSX.Element {
+  return <AbstractAccordion {...props} className={c(Accordion.displayName, props.className)} />
 }
