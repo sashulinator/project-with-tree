0b905b0137b232c0bd774a8d2f1026b0f4c41660
Savchenko Alexander <asavchenko@datatech.ru>
2023-07-28 10:36:20 +0400

    node bg

--- src/ui/canvas/widgets/new-node/themes/light.ts
+++ src/ui/canvas/widgets/new-node/themes/light.ts
@@ -1,6 +1,8 @@
+import { LIGHT } from '~/shared/theme/light'
 import { CSSVars } from '../types/css-vars'
 import { common } from './common'
 
 export const light: CSSVars = {
   ...common,
+  canvasNode_bg: LIGHT.primary.alpha(0.7),
 }
--- src/ui/canvas/widgets/new-node/types/css-vars.ts
+++ src/ui/canvas/widgets/new-node/types/css-vars.ts
@@ -1,2 +1,6 @@
+import { ToStringable } from '~/utils/core'
+
 // eslint-disable-next-line @typescript-eslint/ban-types
-export type CSSVars = {}
+export type CSSVars = {
+  canvasNode_bg: ToStringable
+}
