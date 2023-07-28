c105a6811a02b8d433b6b16e6e4cfe47b049126a
Savchenko Aleksandr <33823497+sashulinator@users.noreply.github.com>
2023-07-28 10:36:39 +0400

    node bg (#103)
    Co-authored-by: Savchenko Alexander <asavchenko@datatech.ru>

--- src/ui/canvas/widgets/new-node/themes/dark.ts
+++ src/ui/canvas/widgets/new-node/themes/dark.ts
@@ -1,6 +1,8 @@
+import { DARK } from '~/shared/theme/dark'
 import { CSSVars } from '../types/css-vars'
 import { common } from './common'
 
 export const dark: CSSVars = {
   ...common,
+  canvasNode_bg: DARK.primary.darken(0.5),
 }
