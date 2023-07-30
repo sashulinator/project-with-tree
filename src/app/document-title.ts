180813bd23342944273af45af9c316e514731d40
Savchenko Aleksandr <33823497+sashulinator@users.noreply.github.com>
2023-07-30 10:21:08 +0400

    feat(DocumentTitle) set `window.projectName` variable in `index.html` (#107)
    Co-authored-by: Savchenko Alexander <asavchenko@datatech.ru>

--- src/app/document-title.ts
+++ src/app/document-title.ts
@@ -3,6 +3,15 @@ import { useLocation } from 'react-router-dom'
 import ROUTES from '~/shared/routes'
 import { matchPath } from 'react-router'
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+const projectName = (window as any).projectName as string
+
+if (projectName === undefined) {
+  throw new Error('`window.projectName` is undefined')
+}
+
+DocumentTitle.displayName = 'app-DocumentTitle'
+
 // Its a component!!
 // It could be a hook but it could effect rerenders
 export default function DocumentTitle(): null {
@@ -11,6 +20,6 @@ export default function DocumentTitle(): null {
     .find((route) => matchPath(route.path, location.pathname))
     ?.getName()
   const name = routeName ? ` | ${routeName}` : ''
-  document.title = `Calibri${name}`
+  document.title = `${projectName}${name}`
   return null
 }
