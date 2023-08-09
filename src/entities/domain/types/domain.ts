f71f654580c7c4954e4a5ccb1ba3b55d5f96e342
Savchenko Alexander <asavchenko@datatech.ru>
2023-08-09 13:59:15 +0400

    feat(Domain) add type

---
+++ src/entities/domain/types/domain.ts
@@ -0,0 +1,65 @@
+import { Id } from '~/utils/core'
+
+export type Type = 'UNKNOWN1' | 'UNKNOWN2'
+
+export interface Domain {
+  /**
+   * Идентификатор
+   */
+  id: Id
+
+  /**
+   * Версия
+   */
+  rev: string
+
+  /**
+   * Имя
+   */
+  name: string
+
+  /**
+   * Cистемное название
+   */
+  keyname: string
+
+  /**
+   * Описание
+   */
+  description: string
+
+  /**
+   * Тип
+   */
+  type: Type
+
+  /**
+   * Родительский домен
+   */
+  parentId: Id
+
+  /**
+   * Cистема источник
+   */
+  sourceSystemId: Id
+
+  /**
+   * Дата создания
+   */
+  createDttm: string
+
+  /**
+   * Дата обновления
+   */
+  updateDttm: string
+
+  /**
+   * Кем создано
+   */
+  createdBy: string
+
+  /**
+   * Кем обновлено
+   */
+  updatesBy: string
+}
