90df00bba1bd48232da6637dc48a8cac4ab68644
Savchenko Alexander <asavchenko@datatech.ru>
2023-08-09 15:06:50 +0400

    ref(RulesCreatePage)

--- src/entities/rules/ui/editor/ui/editor.tsx
+++ src/entities/rules/ui/editor/ui/editor.tsx
@@ -3,21 +3,32 @@ import './editor.css'
 import { DomainItemProps } from '~/entities/rules/types/rules-type'
 import { DomainList } from '../widgets/domain-list'
 import { Rules } from '../widgets/rules'
+import { c } from '~/utils/core'
+import { useSetRecoilState } from 'recoil'
+import { mentionsDataAtom } from '~/entities/rules/models/mentionsData'
+import { addDataMentions } from '~/entities/rules/lib/add-data-mentions'
+import { useEffect } from 'react'
+
+Editor.displayName = 'ruleEditor'
 
 interface AddRulesProps {
   dataList: DomainItemProps[]
+  className?: string
 }
 
-export function Create(props: AddRulesProps): JSX.Element {
+export function Editor(props: AddRulesProps): JSX.Element {
   const { dataList } = props
 
-  return (
-    <main className='e-ui-Rules-Create'>
-      <nav className='list'>
-        <DomainList domains={dataList} defaultExpanded={true} />
-      </nav>
+  const setMentionsData = useSetRecoilState(mentionsDataAtom)
 
+  useEffect(() => setMentionsData(addDataMentions(dataList)), [dataList])
+
+  return (
+    <div className={c(props.className, Editor.displayName)}>
+      <div className='list'>
+        <DomainList domains={dataList} defaultExpanded={true} />
+      </div>
       <Rules />
-    </main>
+    </div>
   )
 }
--- src/pages/rule/create/index.tsx
+++ src/pages/rule/create/index.tsx
@@ -1,35 +1,21 @@
-import { useMemo, useEffect } from 'react'
 import { useQuery } from 'react-query'
-import { useSetRecoilState } from 'recoil'
 import { url, makeRequest } from '~/api/rules/mock/fetch'
-import { addDataMentions } from '~/entities/rules/lib/add-data-mentions'
-import { mentionsDataAtom } from '~/entities/rules/models/mentionsData'
-import { DomainItemProps } from '~/entities/rules/types/rules-type'
-import { Create } from '~/entities/rules/ui/editor'
+
+import { Editor } from '~/entities/rules/ui/editor'
 import mockRules from '~/mocks/rules/mock-rules'
 
 export default function RulesPage(): JSX.Element {
-  const { data, isLoading, isSuccess } = useQuery([url, mockRules.name, { id: mockRules.id }], () =>
+  const { data, isLoading, isSuccess, isError } = useQuery([url, mockRules.name, { id: mockRules.id }], () =>
     makeRequest({ id: mockRules.id })
   )
 
-  const setMentionsData = useSetRecoilState(mentionsDataAtom)
+  const dataList = data?.data.data ?? []
 
-  const dataList: DomainItemProps[] = useMemo(() => {
-    return isSuccess ? data.data.data : []
-  }, [isSuccess])
-
-  useEffect(() => {
-    setMentionsData(addDataMentions(dataList))
-  }, [isSuccess])
-
-  if (isSuccess) {
-    return <Create dataList={dataList} />
-  }
-
-  if (isLoading) {
-    return <div>loading...</div>
-  }
-
-  return <div>ошибка...</div>
+  return (
+    <main>
+      {isError && <div>error...</div>}
+      {isLoading && <div>loading...</div>}
+      {isSuccess && <Editor dataList={dataList} />}
+    </main>
+  )
 }
