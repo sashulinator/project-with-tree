f06271136ef4b11303c1e9c99f4d958ebabcc09d
Savchenko Aleksandr <33823497+sashulinator@users.noreply.github.com>
2023-08-09 13:42:57 +0400

    ref(onGestureDrag) drag node only if movement more than 10px (#136)
    Co-authored-by: Savchenko Alexander <asavchenko@datatech.ru>

--- src/entities/decision/ui/editor/widgets/canvas/lib/on-gesture-drag.ts
+++ src/entities/decision/ui/editor/widgets/canvas/lib/on-gesture-drag.ts
@@ -2,6 +2,13 @@ import { GestureDragEvent } from '~/ui/canvas'
 import { NodeState, getColumnX, getNodeMovement } from '../../..'
 import { State } from '..'
 
+/**
+ * Функция обработки драга ноды
+ * `Gesture` в названии отсылает к библиотечке @use-gesture/react
+ * @param {State} state
+ * @param {NodeState} nodeState
+ * @param {GestureDragEvent} event событие библиотечки @use-gesture/react
+ */
 export function onGestureDrag(state: State) {
   return (nodeState: NodeState) =>
     (event: GestureDragEvent): void => {
@@ -9,6 +16,10 @@ export function onGestureDrag(state: State) {
 
       const movePosition = getNodeMovement(event, state.scale.value)
       if (movePosition === null) return
+
+      // Начинаем двигать карточку только если movePosition больше 10
+      // иначе карточка дергается при простом клике на нее
+      if (Math.abs(movePosition.x) < 10 && Math.abs(movePosition.y) < 10) return
 
       const x = nodeState.position.start.x + movePosition.x
       const y = nodeState.position.start.y + movePosition.y
