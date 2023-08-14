13439b9a5106939e3adae5bc115e8b350aed27b2
Savchenko Alexander <asavchenko@datatech.ru>
2023-08-14 16:22:26 +0400

    ref(Align) ref

--- src/abstract/align/ui/align.tsx
+++ src/abstract/align/ui/align.tsx
@@ -1,20 +1,109 @@
 import { alignElement } from 'dom-align-ts'
+import type { Offset, Points } from 'dom-align-ts'
 import React, { useCallback, useLayoutEffect } from 'react'
 import { createPortal } from 'react-dom'
 
 import { listenParentScrolls, observeResize } from '~/utils/dom'
 import { useEventListener, useLatest } from '~/utils/hooks'
 import { assertValidElement, setRefs } from '~/utils/react'
+import type { ReactElementWithRef } from '~/utils/react'
 
-import { AlignProps } from '../types/align-props'
+Align.displayName = 'a-Align'
+
+/**
+ * Configuration options for handling positioning when the content overflows the container.
+ */
+export interface Overflow {
+  /**
+   * Flag to shift the child element into the viewport until it is entirely visible; defaults to false.
+   */
+  alwaysByViewport?: boolean
+
+  /**
+   * Flag to reposition the child element along the x axis to ensure it is visible; defaults to false.
+   */
+  adjustX?: boolean
+
+  /**
+   * Flag to reposition the child element along the y axis to ensure it is visible; defaults to false.
+   */
+  adjustY?: boolean
+}
+
+export interface Props {
+  /**
+   * The target element to align the child element with.
+   */
+  targetElement: HTMLElement
+
+  /**
+   * The container element for the component; defaults to `document.body`.
+   */
+  containerElement?: HTMLElement | null | undefined
+
+  /**
+   * The child element to be positioned.
+   */
+  children: ReactElementWithRef<HTMLElement>
+
+  /**
+   * An Array that specifies the positioning of the child element relative to the target element.
+   */
+  points: Points
+
+  /**
+   * An optional array of dependencies used to trigger repositioning of the child element.
+   */
+  deps?: unknown[] | undefined
+
+  /**
+   * An optional x/y offset for the child element.
+   */
+  sourceOffset?: Offset | undefined
+
+  /**
+   * An optional x/y offset for the target element
+   */
+  targetOffset?: Offset | undefined
+
+  /**
+   * An optional configuration to handle positioning when the content overflows the container.
+   */
+  overflow?: Overflow | undefined
+
+  /**
+   * An optional flag to use CSS `right` property instead of `left`.
+   */
+  useCssRight?: boolean | undefined
+
+  /**
+   * An optional flag to use CSS `bottom` property instead of `top`.
+   */
+  useCssBottom?: boolean | undefined
+
+  /**
+   * An optional flag to use CSS `transform` property instead of `left`, `top`.
+   */
+  useCssTransform?: boolean | undefined
+
+  /**
+   * An optional flag to ignore shaking of the child element when repositioning.
+   */
+  ignoreShake?: boolean | undefined
+
+  /**
+   *  An optional function to be called after the child element is positioned.
+   */
+  onAligned?: ((ret: ReturnType<typeof alignElement>) => void) | undefined
+}
 
 /**
  * A component for positioning an element relative to another element.
  *
- * @param {AlignProps} props - The props for the Align component.
+ * @param {Props} props - The props for the Align component.
  * @return {JSX.Element} The rendered Align component.
  */
-export default function Align(props: AlignProps): JSX.Element {
+export default function Align(props: Props): JSX.Element {
   const { targetElement, children, containerElement, deps = [], onAligned, sourceOffset, ...config } = props
   const [sourceElement, setSourceEl] = React.useState<null | HTMLElement>(null)
   const onAlignedRef = useLatest(onAligned)
@@ -42,5 +131,3 @@ export default function Align(props: AlignProps): JSX.Element {
     onAlignedRef.current?.(ret)
   }
 }
-
-Align.displayName = 'AbstractAlign'
