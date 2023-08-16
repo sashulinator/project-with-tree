26f07efee3dfcf2aa99408dd2f79eacd841ce647
Savchenko Alexander <asavchenko@datatech.ru>
2023-08-16 13:33:10 +0400

    stryboo(decisionCanvas-w-Node-v-Filter) ref

---
+++ src/storybook/pages/canvas/ui-w-node-v-filter.tsx
@@ -0,0 +1,81 @@
+import { useMemo } from 'react'
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
+
+import { LinkListState, NodeListState } from '~/entities/decision/ui/editor'
+import { FilterNode } from '~/entities/decision/ui/editor/widgets/node'
+import { Point } from '~/entities/point'
+import { Config, Props } from '~/storybook/types'
+
+const point1: Point = {
+  id: 'id1',
+  name: 'name1',
+  type: 'FILTER',
+  computation: 'parallel',
+  x: 10,
+  y: 10,
+}
+const point2: Point = {
+  id: 'id2',
+  name: 'name2',
+  type: 'FILTER',
+  computation: 'parallel',
+  x: 510,
+  y: 10,
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface State {}
+
+export default {
+  getName: (): string => FilterNode.displayName || '',
+
+  getPath: (): string => `/canvas-w-item`,
+
+  getDescription: (): JSX.Element | string => {
+    return 'Размер `foreignObject` подстраивается под размер детей'
+  },
+
+  element: function Element(props: Props<State>): JSX.Element {
+    const states = useMemo(() => new NodeListState([point1, point2]), [])
+    const linkStates = useMemo(
+      () =>
+        new LinkListState([
+          {
+            id: 'id1',
+            name: 'name',
+            sourceId: 'id1',
+            targetId: 'id2',
+            i: 1,
+          },
+        ]),
+      []
+    )
+
+    return (
+      <svg width='100%' height='333px' style={{ border: '1px solid red' }}>
+        <DndProvider backend={HTML5Backend}>
+          {states.values().map((state) => {
+            return (
+              <FilterNode
+                {...props}
+                nodeListState={states}
+                key={state.id}
+                state={state}
+                linkListState={linkStates}
+                remove={(): void => console.log('remove!')}
+                onGestureDrug={(event): void => {
+                  const x = state.position.start.x + event.movement[0]
+                  const y = state.position.start.y + event.movement[1]
+                  state.position.move({ x, y }, event)
+                }}
+              />
+            )
+          })}
+        </DndProvider>
+      </svg>
+    )
+  },
+
+  controls: [],
+} satisfies Config<State>
