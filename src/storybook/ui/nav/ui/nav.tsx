75c36dbc2fa35e0992d7e5e1a857b5a1faf79ad4
Savchenko Alexander <asavchenko@datatech.ru>
2023-08-16 18:47:21 +0400

    ref(storybook-Align) small fix

--- src/storybook/ui/nav/ui/nav.tsx
+++ src/storybook/ui/nav/ui/nav.tsx
@@ -5,7 +5,6 @@ import getRootElement from '~/lib/dom/get-root-element'
 import { configToPath } from '~/storybook/lib'
 import { routes } from '~/storybook/routes'
 import { Config } from '~/storybook/types'
-import Link from '~/ui/link'
 import ThemeDropdown from '~/ui/theme-dropdown'
 import { Any } from '~/utils/core'
 import { setCSSVar } from '~/utils/dom'
@@ -16,7 +15,13 @@ export default function Nav(): JSX.Element {
   setCSSVar('nav-width', 200, getRootElement())
 
   return (
-    <nav style={{ borderRight: '1px solid var(--bgSecondary)' }}>
+    <nav
+      style={{
+        borderRight: '1px solid var(--bgSecondary)',
+        height: 'calc(100vh - var(--header-height) * 1px)',
+        overflow: 'scroll',
+      }}
+    >
       <Flex gap='xxxl' dir='column' padding='var(--xxxl)'>
         <ThemeDropdown />
         <ul style={{ width: '100%' }}>
@@ -25,13 +30,13 @@ export default function Nav(): JSX.Element {
               {routes.flatMap(([name, ...configs]) => {
                 return (
                   <Fragment key={name}>
-                    <li>{name}</li>
+                    <li style={{ marginTop: 'var(--l)' }}>{name}</li>
                     <ul>
                       {configs.map((c: unknown) => {
                         const config = c as Config<Any>
                         const path = configToPath(config)
                         return (
-                          <li key={path}>
+                          <li key={path} style={{ marginTop: 'var(--m)' }}>
                             <ConfigLink config={config} />
                           </li>
                         )
