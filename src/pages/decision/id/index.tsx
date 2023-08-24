1cd5a03d68aeb5065a237ede90f8567cf439c01c
Savchenko Alexander <asavchenko@datatech.ru>
2023-08-24 16:36:48 +0400

    ref(decision-Editor) update mock

--- src/pages/decision/id/index.tsx
+++ src/pages/decision/id/index.tsx
@@ -2,7 +2,8 @@ import './index.css'
 
 import { useParams } from 'react-router-dom'
 
-import { useFetchDecision } from '~/api/decision/fetch'
+// import { useFetchDecision } from '~/api/decision/fetch'
+import { useFetchDecisionMock } from '~/api/decision/fetch-mock'
 import { useUpdateDecision } from '~/api/decision/update'
 import { Decision, Editor } from '~/entities/decision'
 import { notify } from '~/shared/notify'
@@ -12,7 +13,8 @@ export default function DecisionPage(): JSX.Element {
   const { id } = useParams<{ id: string }>()
 
   assertDefined(id)
-  const fetcher = useFetchDecision({ id })
+  const fetcher = useFetchDecisionMock({ id })
+  // const fetcher = useFetchDecision({ id })
 
   const mutator = useUpdateDecision({
     onSuccess: () => notify({ data: 'Сохранено', type: 'success' }),
