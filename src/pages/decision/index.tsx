747122da567e057b0050e51286cbc977693e08da
Savchenko Alexander <asavchenko@datatech.ru>
2023-08-29 15:33:03 +0400

    add remove tree

--- src/pages/decision/index.tsx
+++ src/pages/decision/index.tsx
@@ -1,19 +1,36 @@
 import './index.css'
 
+import Flex from '~/abstract/flex/ui/flex'
 import { useFetchDecisionList } from '~/api/decision/fetch-list'
+import { useRemoveDecision } from '~/api/decision/remove'
 import { List } from '~/entities/decision/ui/item'
+import { notify } from '~/shared/notify'
 import { routes } from '~/shared/routes'
 import Link from '~/ui/link'
+import { Id } from '~/utils/core'
 
 export default function DecisionPage(): JSX.Element {
   const fetcher = useFetchDecisionList({ page: 1, limit: 30 })
+  const removeMutator = useRemoveDecision({
+    onSuccess: () => {
+      notify({ data: 'Удалено', type: 'success' })
+      void fetcher.refetch()
+    },
+    onError: () => notify({ data: 'Ошибка', type: 'error' }),
+  })
 
   return (
     <main className='DecisionListPage'>
-      <Link to={routes.decisionCreate.getURL()}>Создать</Link>
-      <List className='decisionList' list={fetcher.data?.items || []} />
+      <Flex margin='0 0 var(--xxl)'>
+        <Link to={routes.decisionCreate.getURL()}>Создать</Link>
+      </Flex>
+      <List className='decisionList' list={fetcher.data?.items || []} remove={remove} />
     </main>
   )
 
   // Private
+
+  function remove(id: Id): void {
+    removeMutator.mutate({ id })
+  }
 }
