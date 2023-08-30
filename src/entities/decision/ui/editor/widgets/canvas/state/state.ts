8e2e652064f0e91225e967e918c2ba1c1d0bae8c
Savchenko Aleksandr <33823497+sashulinator@users.noreply.github.com>
2023-08-30 19:54:57 +0400

    ref(zoom-prop) zoom (#222)
    Co-authored-by: Savchenko Alexander <asavchenko@datatech.ru>

--- src/entities/decision/ui/editor/widgets/canvas/state/state.ts
+++ src/entities/decision/ui/editor/widgets/canvas/state/state.ts
@@ -1,27 +1,20 @@
-import { PositionProp } from '~/lib/emitter'
+import { Zoom, ZoomProp } from '~/lib/emitter'
 import { Position } from '~/utils/core'
+import { isMetaCtrlKey } from '~/utils/dom-event'
 import { Size } from '~/utils/dom/types/size'
 import { Emitter, Prop } from '~/utils/emitter'
 
-import { D3Selection } from './d3-selection'
-import { D3Zoom } from './d3-zoom'
+import { getLocalStorageZoom, setZoomLocalStorage } from '../lib/local-storage-zoom'
 
 export type Events = {
-  scale: { value: number }
-  translate: { value: Position }
+  zoom: { value: Zoom }
   ref: { element: SVGSVGElement }
 }
 
 export class State extends Emitter<Events> {
   ref: Prop<'ref', null | SVGSVGElement>
 
-  translate: PositionProp<'translate'>
-
-  scale: Prop<'scale', number>
-
-  d3selection: D3Selection<Events>
-
-  d3zoom: D3Zoom<Events>
+  zoom: ZoomProp<'zoom'>
 
   highlight: Prop<'highlight', (Position & Size) | null>
 
@@ -30,14 +23,19 @@ export class State extends Emitter<Events> {
 
     this.ref = new Prop('ref', null as null | SVGSVGElement, this)
 
-    this.translate = new PositionProp('translate', { x: 0, y: 0 }, this)
-
-    this.scale = new Prop('scale', 1, this)
-
-    this.d3selection = new D3Selection(this)
-
-    this.d3zoom = new D3Zoom(this)
-
     this.highlight = new Prop('highlight', null as (Position & Size) | null, this)
+
+    // TODO рассчитать середину main ноды
+    this.zoom = new ZoomProp('zoom', getLocalStorageZoom() || { x: 500, y: 500, k: 1 }, this, {
+      onChange: (value): void => {
+        setZoomLocalStorage(value)
+      },
+      filter: (event): boolean => {
+        // На Ctrl мы даем возможность пользователю выделять ноды
+        if (isMetaCtrlKey(event)) return false
+        if (event.type === 'wheel') return true
+        return event.target === this.ref.value
+      },
+    })
   }
 }
