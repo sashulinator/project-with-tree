de85e1d30a9da74a3d7033d59fa030265532f98d
Savchenko Alexander <asavchenko@datatech.ru>
2023-09-03 21:47:35 +0400

    feat(env) add

--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,39 @@
 import react from '@vitejs/plugin-react-swc'
 
 import path from 'path'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import checker from 'vite-plugin-checker'
 
 const vendors = [`react`, `react-dom`]
 
-export default defineConfig({
-  plugins: [react(), checker({ typescript: true })],
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: vendors,
-          packages: ['i18next'],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react(), checker({ typescript: true })],
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vendor: vendors,
+            packages: ['i18next'],
+          },
+        },
+        onLog(level, log, handler) {
+          if (log.code === 'CYCLIC_CROSS_CHUNK_REEXPORT') return // Ignore circular dependency warnings
+          handler(level, log)
         },
       },
-      onLog(level, log, handler) {
-        if (log.code === 'CYCLIC_CROSS_CHUNK_REEXPORT') return // Ignore circular dependency warnings
-        handler(level, log)
+    },
+    resolve: {
+      alias: {
+        '~': path.resolve(__dirname, './src'),
       },
     },
-  },
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './src'),
+    server: {
+      proxy: {
+        '/api': env.URL,
+      },
     },
-  },
-  server: {
-    proxy: {
-      '/api': 'http://10.4.87.16:8084',
-    },
-  },
+  }
 })
