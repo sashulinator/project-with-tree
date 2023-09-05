ce10be4b556bb2f42ca6ee66f897628bc8600d28
Savchenko Alexander <asavchenko@datatech.ru>
2023-09-05 21:12:37 +0400

    ref(ui-Paper) remove

--- src/ui/paper/themes/dark.ts
+++
@@ -1,9 +0,0 @@
-import { DARK } from '~/shared/theme/dark'
-import { CSSVars } from '../types/css-vars'
-import { common } from './common'
-
-export const dark: CSSVars = {
-  ...common,
-  paper_bg: DARK.bgSecondary,
-  borderColor: DARK.bgSecondary.lighten(0.7),
-}
