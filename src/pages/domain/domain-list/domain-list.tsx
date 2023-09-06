ed2cbb187ebd916a6ec6697bc317afa82a5a2fe4
Savchenko Alexander <asavchenko@datatech.ru>
2023-09-06 14:39:52 +0400

    ref(entities/rules) remove

--- src/pages/domain/domain-list/domain-list.tsx
+++ src/pages/domain/domain-list/domain-list.tsx
@@ -19,6 +19,7 @@ export default function DomainListPage(): JSX.Element {
 
         {isAddAttributeActive.isActive && (
           <AddAttribute
+            key={isAddDomainActive.parentId}
             opened={isAddAttributeActive.isActive}
             fetcher={fetcher}
             domainId={isAddAttributeActive.domainId}
