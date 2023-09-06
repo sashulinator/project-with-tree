a552ec8bcfef6a2821584654b59203de6cf0d3d5
Savchenko Alexander <asavchenko@datatech.ru>
2023-09-06 17:52:48 +0400

    fix(pages/domains) replace `label` to `Labeled`

--- src/entities/domain/ui/add-attribute/ui/add-attribute.tsx
+++ src/entities/domain/ui/add-attribute/ui/add-attribute.tsx
@@ -1,10 +1,10 @@
-import './add-attribute.css'
-
 import { useForm } from 'react-hook-form'
 
+import Flex from '~/abstract/flex'
 import { CreateAttribute } from '~/api/attribute/requests/create'
 import { PrimaryButton } from '~/ui/button'
 import Input from '~/ui/input'
+import Labeled from '~/ui/labeled/ui/labeled'
 import Modal from '~/ui/modal'
 import { Id, c } from '~/utils/core'
 
@@ -15,7 +15,7 @@ interface Props {
   create: (attribute: CreateAttribute) => void
 }
 
-export function AddAttribute(props: Props): JSX.Element {
+export function AddAttribute(props: Props): JSX.Element | null {
   const {
     register,
     handleSubmit,
@@ -23,69 +23,82 @@ export function AddAttribute(props: Props): JSX.Element {
     formState: { errors },
   } = useForm<CreateAttribute>()
 
-  return (
-    <Modal opened={props.opened} onDismiss={props.close}>
-      {/* eslint-disable-next-line @typescript-eslint/no-misused-promises*/}
-      <form onSubmit={handleSubmit(onSubmit)} style={{ minWidth: '600px' }}>
-        <label>
-          Имя атрибута
-          <Input
-            className={c('add-attribute-input', errors.name && '--error')}
-            {...register('name', { required: true })}
-          />
-          {errors.name && <span>Обязательное поле</span>}
-        </label>
-        <label>
-          keyName атрибута
-          <Input
-            className={c('add-attribute-input', errors.keyName && '--error')}
-            {...register('keyName', { required: true })}
-          />
-          {errors.keyName && <span>Обязательное поле</span>}
-        </label>
-        <label>
-          Описание атрибута
-          <Input
-            className={c('add-attribute-input', errors.description && '--error')}
-            {...register('description', { required: true })}
-          />
-          {errors.description && <span>Обязательное поле</span>}
-        </label>
-        <label>
-          Тип атрибута
-          <Input
-            defaultValue={'string'}
-            className={c('add-attribute-input', errors.type && '--error')}
-            {...register('type', { required: true })}
-          />
-          {errors.type && <span>Обязательное поле</span>}
-        </label>
-        <label>
-          {'Id домена'}
-          <Input
-            readOnly
-            defaultValue={props.domainId}
-            className={c('add-attribute-input', errors.domainId && '--error')}
-            {...register('domainId', { required: true })}
-          />
-        </label>
-        <label>
-          userId пользователя
-          <Input
-            defaultValue={'user@mail.ru'}
-            className={c('add-attribute-input', errors.userId && '--error')}
-            {...register('userId', { required: true })}
-          />
-          {errors.userId && <span>Обязательное поле</span>}
-        </label>
+  if (props.domainId === null) return null
 
-        <PrimaryButton type='submit'>Создать</PrimaryButton>
-      </form>
+  return (
+    <Modal firstFocused={true} opened={props.opened} onDismiss={props.close}>
+      <Flex width='25rem' dir='column'>
+        <Flex
+          as='form'
+          dir='column'
+          crossAxis='stretch'
+          gap='xl'
+          // eslint-disable-next-line @typescript-eslint/no-misused-promises
+          onSubmit={handleSubmit(props.create)}
+          width='100%'
+        >
+          <div>
+            <Labeled label='Имя'>
+              <Input
+                className={c('add-attribute-input', errors.name && '--error')}
+                {...register('name', { required: true })}
+              />
+            </Labeled>
+            {errors.name && <span>Обязательное поле</span>}
+          </div>
+          <div>
+            <Labeled label='keyName'>
+              <Input
+                className={c('add-attribute-input', errors.keyName && '--error')}
+                {...register('keyName', { required: true })}
+              />
+            </Labeled>
+            {errors.keyName && <span>Обязательное поле</span>}
+          </div>
+          <div>
+            <Labeled label='Описание'>
+              <Input
+                className={c('add-attribute-input', errors.description && '--error')}
+                {...register('description', { required: true })}
+              />
+            </Labeled>
+            {errors.description && <span>Обязательное поле</span>}
+          </div>
+          <div>
+            <Labeled label='Тип'>
+              <Input
+                defaultValue={'string'}
+                className={c('add-attribute-input', errors.type && '--error')}
+                {...register('type', { required: true })}
+              />
+            </Labeled>
+            {errors.type && <span>Обязательное поле</span>}
+          </div>
+          <div>
+            <Labeled label='Id домена'>
+              <Input
+                readOnly
+                defaultValue={props.domainId}
+                className={c('add-attribute-input', errors.domainId && '--error')}
+                {...register('domainId', { required: true })}
+              />
+            </Labeled>
+          </div>
+          <div>
+            <Labeled label='Id пользователя'>
+              <Input
+                defaultValue={'user@mail.ru'}
+                className={c('add-attribute-input', errors.userId && '--error')}
+                {...register('userId', { required: true })}
+              />
+            </Labeled>
+            {errors.userId && <span>Обязательное поле</span>}
+          </div>
+          <Flex mainAxis='end'>
+            <PrimaryButton type='submit'>Создать</PrimaryButton>
+          </Flex>
+        </Flex>
+      </Flex>
     </Modal>
   )
-
-  //Private
-  function onSubmit(data: CreateAttribute): void {
-    props.create(data)
-  }
 }
