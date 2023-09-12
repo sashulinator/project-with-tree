e25fd5d5c52a1b9e19766fdb161f5e62e9f9584e
Николаев Вячеслав Андреевич <VANikolaev@inno.local>
2023-09-12 17:05:24 +0300

    fix(editor/list)

--- src/entities/rule/ui/list/widget/rule-item/rule-item.tsx
+++ src/entities/rule/ui/list/widget/rule-item/rule-item.tsx
@@ -34,13 +34,16 @@ export default function RuleItem(props: Props): JSX.Element {
   return (
     <div className={c(props.className, RuleItem.displayName)}>
       <Flex mainAxis='space-between' crossAxis='center' className='nameCell'>
-        <Link to={routes.ruleUpdate.path.replace(':id', props.item?.id.toString() || '')}>{props.item?.name}</Link>
-        <GhostButton onClick={onDelete}>
-          <Close></Close>
-        </GhostButton>
+        <Link
+          to={routes.ruleUpdate.path.replace(':id', props.item?.id.toString() || '')}
+        >{`${props.item?.name} (${props.item.keyName})`}</Link>
+        <Flex gap='xxl' crossAxis='center'>
+          <div>{props.item?.updatedBy}</div>
+          <GhostButton onClick={onDelete}>
+            <Close></Close>
+          </GhostButton>
+        </Flex>
       </Flex>
-
-      <div className={c('statusCell', `--${props.item?.rev.toLowerCase()}`)}>{props.item?.updatedBy}</div>
     </div>
   )
 
