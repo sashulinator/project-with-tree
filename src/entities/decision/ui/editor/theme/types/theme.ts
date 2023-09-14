b3bc2cf7fe46627c3aac40503bb90d92a4f3b192
Savchenko Alexander <asavchenko@datatech.ru>
2023-09-14 18:36:13 +0400

    ref(decision-Editor) joint

--- src/entities/decision/ui/editor/theme/types/theme.ts
+++ src/entities/decision/ui/editor/theme/types/theme.ts
@@ -3,4 +3,7 @@ import { ToStringable } from '~/utils/core'
 export interface Theme {
   'decision-Editor_bg': ToStringable
   'decision-Editor-panel_bg': ToStringable
+  'decision-Editor-joint_color__unlinked': ToStringable
+  'decision-Editor-joint_color__linked': ToStringable
+  'decision-Editor-joint_color__new': ToStringable
 }
--- src/entities/decision/ui/editor/widgets/canvas/widgets/node/widgets/joint/ui/joint.tsx
+++ src/entities/decision/ui/editor/widgets/canvas/widgets/node/widgets/joint/ui/joint.tsx
@@ -1,14 +1,8 @@
 import './joint.css'
 
-import { emitter } from '~/shared/emitter'
 import { Id, c } from '~/utils/core'
 
-import { dark } from '../themes/dark'
-import { light } from '../themes/light'
-
 Joint.displayName = 'decision-Editor-w-Canvas-w-Node-w-Joint'
-
-emitter.emit('addThemes', { dark, light })
 
 export interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   linkId: Id
