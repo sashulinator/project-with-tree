6c68a72b56d906c4b4628f7dfaabc43286a52981
Savchenko Alexander <asavchenko@datatech.ru>
2023-09-14 19:01:21 +0400

    ref(decision-Editor) remove spaces

--- src/entities/decision/ui/editor/theme/models/dark.ts
+++ src/entities/decision/ui/editor/theme/models/dark.ts
@@ -6,9 +6,9 @@ import { Theme } from '..'
 
 // prettier-ignore
 export const dark = {
-  'decision-Editor_bg':                                       DARK.bg,
-  'decision-Editor-panel_bg':                                 DARK.bg.darken(0.2).alpha(0.95),
-  'decision-Editor-joint_color__unlinked':     DARK.errorColor,
-  'decision-Editor-joint_color__linked':       clr('rgb(83, 160, 255)'),
-  'decision-Editor-joint_color__new':          clr('rgb(84, 222, 104)'),
+  'decision-Editor_bg':                                   DARK.bg,
+  'decision-Editor-panel_bg':                             DARK.bg.darken(0.2).alpha(0.95),
+  'decision-Editor-joint_color__unlinked':                DARK.errorColor,
+  'decision-Editor-joint_color__linked':                  clr('rgb(83, 160, 255)'),
+  'decision-Editor-joint_color__new':                     clr('rgb(84, 222, 104)'),
 } satisfies Theme
