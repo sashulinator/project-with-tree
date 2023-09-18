bd9ed0f92eaf556c30bac5059c4e739478e81430
Savchenko Alexander <asavchenko@datatech.ru>
2023-09-18 16:05:31 +0400

    ref(decision-Editor) right panel

--- src/entities/decision/ui/editor/widgets/right-panel/widgets/rule/list/ui/list.tsx
+++ src/entities/decision/ui/editor/widgets/right-panel/widgets/rule/list/ui/list.tsx
@@ -2,6 +2,7 @@ import './list.scss'
 
 import { useState } from 'react'
 
+import Flex from '~/abstract/flex'
 import { RulesRes } from '~/entities/rule/types/rules-type'
 import Input from '~/ui/input/ui/input'
 import { Id, c } from '~/utils/core'
@@ -20,11 +21,16 @@ export default function List(props: Props): JSX.Element {
   const [query, setQuery] = useState('')
 
   const filteredList = props.list.filter((rule) => {
-    return rule.keyName?.toUpperCase().indexOf(query.toUpperCase()) !== -1
+    return (
+      rule.keyName?.toUpperCase().indexOf(query.toUpperCase()) !== -1 ||
+      rule.name?.toUpperCase().indexOf(query.toUpperCase()) !== -1
+    )
   })
   return (
     <div className={c(props.className, List.displayName)}>
-      <Input value={query} onChange={(e): void => setQuery(e.currentTarget.value)} />
+      <Flex width='100%' margin='0 0 var(--l) 0'>
+        <Input value={query} onChange={(e): void => setQuery(e.currentTarget.value)} />
+      </Flex>
       <ul className='list' style={{ display: 'flex', gap: 'var(--l)', overflow: 'scroll' }}>
         {filteredList.map((rule) => {
           return (
