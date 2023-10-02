02fe50769f6ed6ab3e6d96afe672b1634c89398a
Николаев Вячеслав Андреевич <VANikolaev@inno.local>
2023-10-02 14:12:09 +0300

    диалог на редактирование и создание правила

--- src/pages/rules-test/update/update.tsx
+++ src/pages/rules-test/update/update.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 import { useFetchParentDomainList } from '~/api/domain/fetch-parent-domains'
@@ -6,6 +7,7 @@ import { useUpdateRule } from '~/api/rules/update'
 import { EditorValues } from '~/entities/rule-test/types/type'
 import Editor from '~/entities/rule-test/ui/editor'
 import { notify } from '~/shared/notify'
+import Dialog from '~/ui/modal/variants/dialog/ui/dialog'
 
 export default function RulesTestUpdatePage(): JSX.Element {
   const { id } = useParams()
@@ -23,24 +25,39 @@ export default function RulesTestUpdatePage(): JSX.Element {
 
   const fetcher = useFetchParentDomainList({ page: 1, limit: 2000 })
 
+  const [openModal, setOpenModal] = useState<{ editorValue: EditorValues[]; name: string; keyName: string } | null>(
+    null
+  )
+
   return (
     <main style={{ maxWidth: '1400px', margin: '0 auto', width: '100%' }}>
+      {/* Редактирование правила  */}
+      <Dialog
+        firstFocused={true}
+        text={`Вы уверены что хотите изменить правило: ${openModal?.name}?`}
+        onSubmit={updateRule}
+        opened={openModal}
+        onDismiss={(): void => setOpenModal(null)}
+      />
       {(data.isLoading || id !== data.data?.data.id) && <div>Загрузка</div>}
       {data.isError || (fetcher.isError && <div>Ошибка</div>)}
       {data.isSuccess && fetcher.isSuccess && id === data.data?.data.id && (
-        <Editor rule={data.data?.data} onSubmit={onSubmit} dataList={fetcher.data?.items} />
+        <Editor rule={data.data?.data} handleOpenModal={setOpenModal} dataList={fetcher.data?.items} />
       )}
     </main>
   )
 
   // Private
-  function onSubmit(editorValue: EditorValues[], name: string, keyName: string): void {
-    updateRuleMutation.mutate({
-      id: id as string,
-      name: name,
-      keyName: keyName,
-      frontValue: editorValue,
-      userId: 'user@mail.ru',
-    })
+  function updateRule(): void {
+    if (openModal) {
+      updateRuleMutation.mutate({
+        id: id as string,
+        name: openModal.name,
+        keyName: openModal.keyName,
+        frontValue: openModal.editorValue,
+        userId: 'user@mail.ru',
+      })
+      setOpenModal(null)
+    }
   }
 }
